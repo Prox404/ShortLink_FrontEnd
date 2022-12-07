0a64551e1cd27bd5fa73989a17c7b3d6e847af61
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-12-08 03:02:55 +0700

    fix ui dashboard

--- src/layouts/Components/Sidebar/Sidebar.js
+++ src/layouts/Components/Sidebar/Sidebar.js
@@ -1,8 +1,8 @@
 import classNames from "classnames/bind";
 import { RiDashboardFill } from "react-icons/ri";
 import { FaLink } from "react-icons/fa";
-import {  MdInfo } from "react-icons/md";
-import {  AiFillBook } from "react-icons/ai";
+import { MdInfo } from "react-icons/md";
+import { AiFillBook } from "react-icons/ai";
 
 import SidebarItem from "~/components/SidebarItem"
 import styles from "./Sidebar.module.scss";
@@ -12,22 +12,22 @@ const content = [
     {
         title: "Dashboard",
         to: "/",
-        icon: <RiDashboardFill/>,   
+        icon: <RiDashboardFill />,
     },
     {
         title: "Link",
         to: "/news",
-        icon: <FaLink/>,
+        icon: <FaLink />,
     },
     {
         title: "Contact",
         to: "/contact",
-        icon: <AiFillBook/>,
+        icon: <AiFillBook />,
     },
     {
         title: "About",
         to: "/about",
-        icon: <MdInfo/>,
+        icon: <MdInfo />,
     }
 ]
 
@@ -35,11 +35,25 @@ function Sidebar() {
 
     return <>
         <div className={cx('wrapper')}>
-            {
-                content.map((item, index) => {
-                    return <SidebarItem key={index} {...item} />
-                })
-            }
+            <div className={cx('logo-wrapper')}>
+                <img className={cx('logo')} src="https://i.ibb.co/kMkJydd/Prox-logo.png" alt="logo" />
+            </div>
+            <div className={cx('sidebar-item-wrapper')}>
+                {
+                    content.map((item, index) => {
+                        return <SidebarItem key={index} {...item} />
+                    })
+                }
+            </div>
+
+
+            <div className={cx('profile-wrapper')}>
+                <SidebarItem
+                    title="Log out"
+                    to="/logout"
+                    icon={<MdInfo />}
+                />
+            </div>
         </div>
 
     </>;
