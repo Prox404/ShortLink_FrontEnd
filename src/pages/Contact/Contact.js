076aeea0ac8291373a502b44931d7da49a3e6375
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-12-26 00:47:33 +0700

    update contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,6 @@
 import classNames from "classnames/bind";
+import { useState } from "react";
+import { toast } from "react-toastify";
 import { RiPhoneFill,RiFacebookLine, RiGithubLine, RiInstagramLine, RiMailFill, RiMapPin2Fill   } from "react-icons/ri";
 
 import styles from "./Contact.module.scss";
@@ -6,6 +8,37 @@ import styles from "./Contact.module.scss";
 const cx = classNames.bind(styles);
 
 function Contact() {
+
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const validate = () => {
+        const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (!email_regex.test(email)) {
+            toast.error("Email is not valid");
+            return false;
+        } else if (email.length === 0) {
+            toast.error("Email is required");
+            return false;
+        } else if (subject.length === 0) {
+            toast.error("Subject is required");
+            return false;
+        } else if (message.length === 0) {
+            toast.error("Message is required");
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmit = async () => {
+        if (validate()) {
+            let sendURL = "mailto:trancongtri@gmail.com?subject=" + subject + "&body=" + message;
+            window.location.href = sendURL;
+        }
+    };
+
     return ( <>
         <h1 className="mb-4">Contact</h1>
         <div className="row">
@@ -62,17 +95,17 @@ function Contact() {
                     <div className={`col-12 col-lg-6 card ${cx('card-right')}`}>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" className="form-control" id="email" placeholder="Enter your email"/>
+                            <input value={email} onChange={setEmail} type="email" className="form-control" id="email" placeholder="Enter your email"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="subject">Subject</label>
-                            <input type="text" className="form-control" id="subject" placeholder="Enter your subject"/>
+                            <input value={subject} onChange={setSubject} type="text" className="form-control" id="subject" placeholder="Enter your subject"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="message">Message</label>
-                            <textarea className="form-control" id="message" rows="3"></textarea>
+                            <textarea value={message} onChange={setMessage} className="form-control" id="message" rows="3"></textarea>
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" onClick={()=>handleSubmit()} className="btn btn-primary">Submit</button>
                     </div>
                 </div>
             </div>
